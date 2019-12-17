553dff498411fe27fb3d3170be9ff55df021652e
emma <emma.wishart@live.com.au>
2019-12-17 15:00:15 +0100

    I watch out for suntax erros - thanks sicco!

--- js-complete-course/02-maths/02-calculator-two/script.js
+++ js-complete-course/02-maths/02-calculator-two/script.js
@@ -9,10 +9,36 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-function test() {
-    return true;
+function Cal (calc) {
+
+    let num1 = document.getElementById("op-one").value;
+    let num2 = document.getElementById("op-two").value;
+
+    let result;
+
+    //let sums = prompt ("Please enter your age.");
+    switch (calc) {
+        case 'addition':
+            result = parseInt(num1) + parseInt(num2);
+            alert(result);
+            break;
+        case 'subtraction':
+            result = parseInt(num1) - parseInt(num2);
+            alert(result);
+            break;
+        case 'multiplication':
+            result = parseInt(num1) * parseInt(num2);
+            alert(result);
+            break;
+        case 'division':
+            result = parseInt(num1) / parseInt(num2);
+            alert(result);
+            break;
+        default:
+            alert("please only enter numbers");
+    }
+
+
+
 }
 
-(function() {
-    return true;
-})();
