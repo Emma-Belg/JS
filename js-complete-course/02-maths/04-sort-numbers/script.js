e229cb950ad04de80c99930a12fee14c1ebc6f20
emma <emma.wishart@live.com.au>
2019-12-18 09:49:17 +0100

    sorting is normally alphabetical, therefore you have to add the compareFunction to make it see which number is actually larger

--- js-complete-course/02-maths/04-sort-numbers/script.js
+++ js-complete-course/02-maths/04-sort-numbers/script.js
@@ -9,14 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // to get the value of an input: document.getElementById("element-id").value
+
+
+
+(function () {
 
     document.getElementById("run").addEventListener("click", function() {
-
-        // your code here
-
+        var points = [2, 4, 14, 10, 90, 23, 16];
+        points.sort(function(a, b) {
+            return a-b
+        });
+        alert(points);
     });
 
 })();
