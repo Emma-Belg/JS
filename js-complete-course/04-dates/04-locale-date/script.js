61f13477264bd936c1cb8d3dd51e61842d98468c
emma <emma.wishart@live.com.au>
2019-12-19 15:52:01 +0100

    I don't know why var d is considered redundant

--- js-complete-course/04-dates/04-locale-date/script.js
+++ js-complete-course/04-dates/04-locale-date/script.js
@@ -11,8 +11,7 @@
 
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
+    let d = new Date();
+    document.getElementById("target").innerHTML = d;
 
 })();
