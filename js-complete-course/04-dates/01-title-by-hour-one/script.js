de95e31bc7de8120f947d8a8a5f2f8f389d50343
emma <emma.wishart@live.com.au>
2019-12-19 09:39:35 +0100

    I feel like Im on the right track but atm the innerHTML is printing the whole function instead of just the if/else conditions within it

--- js-complete-course/04-dates/01-title-by-hour-one/script.js
+++ js-complete-course/04-dates/01-title-by-hour-one/script.js
@@ -12,7 +12,28 @@
 (function() {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    document.getElementById("target").innerHTML = function printDate() {
 
-    // your code here
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    function printDate() {
+        let d = new Date();
+        let h = addZero(d.getHours());
+        if (h() < 18) {
+            let x = document.getElementById("target");
+            x.innerHTML = "Hello!";
+        }
+        else (h() >= 18);{
+            let y = document.getElementById("target");
+            y.innerHTML = "Good evening";
+        }
+    }
+
+            }
 
 })();
