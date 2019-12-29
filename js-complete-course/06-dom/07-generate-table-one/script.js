ebaad2d989b213cd996587e92cfa2b3fe3a6044a
emma <emma.wishart@live.com.au>
2019-12-29 17:54:05 +0100

    it's not working but Im not sure Ill fix it tonight so I will just add now

--- js-complete-course/06-dom/07-generate-table-one/script.js
+++ js-complete-course/06-dom/07-generate-table-one/script.js
@@ -11,6 +11,28 @@
 
 (function() {
 
-    // your code here
+    let newtable = document.createElement("TABLE");
+    let tablecol = document.createElement("TD");
+    document.getElementById("target").appendChild(newtable);
+    document.getElementById("target").appendChild(tablecol);
+
+    rows = [];
+    for (i = 0; i < 10; i++) {
+        document.createElement("TR");
+    }
+    
+    let row = 0;
+    for (let t = 0; t < 9; t++) {
+     row = rows[t];
+    }
+
+
+/*
+let c = document.getElementsByClassName("material");
+let newimg = document.createElement("IMG");
+newimg.src = "http://www.randomkittengenerator.com/cats/rotator.php";
+document.getElementById("target").appendChild(newimg);
+
+ */
 
 })();
