e7b1288377f9e9e460b25b5a50f55a93be3a2823
emma <emma.wishart@live.com.au>
2019-12-29 17:00:43 +0100

    this confused me but I figured it out! You must use CAPITALS for the TAG NAME when creating an element in js, I dont know why

--- js-complete-course/06-dom/04-attr-create/script.js
+++ js-complete-course/06-dom/04-attr-create/script.js
@@ -1,16 +1,50 @@
 /* becode/javascript
- *
  * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
- *
  * coded by leny@BeCode
  * started at 26/10/2018
+ *
+ I would like to keep this as notes to self
+
+    let c = document.getElementsByClassName("material");
+    c.document.getElementById("source").innerHTML = "testing";
+    c.document.replaceChild('source', 'target');
+
+ *
+ // start of other possible solution part
+    var element, newElement, parent;
+
+// Get the original element
+    element = document.getElementById("logo");
+
+// Assuming it exists...
+    if (element) {
+        // Get its parent
+        parent = element.parentNode;
+        // Create the new element
+        newElement = document.createElement('div');
+
+        // Set its ID and content
+        newElement.id = "logo";
+        newElement.innerHTML = "-new content here-";
+
+        // Insert the new one in front of the old one (this temporarily
+        // creates an invalid DOM tree [two elements with the same ID],
+        // but that's harmless because we're about to fix that).
+        parent.insertBefore(newElement, element);
+
+        // Remove the original
+        parent.removeChild(element);
+    }
  */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+(function () {
 
-(function() {
+ //Parent Element is in a section tag with a class="material"
 
-    // your code here
+    let newimg = document.createElement("IMG");
+    newimg.src = "http://www.randomkittengenerator.com/cats/rotator.php";
+    newimg.appendChild(document.getElementById("source"));
+    document.getElementById("target").appendChild(newimg);
+
 
 })();
