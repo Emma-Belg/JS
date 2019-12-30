cb3007d8965c19a37d001a507fa9e7c340e2f4d6
emma <emma.wishart@live.com.au>
2019-12-30 17:00:13 +0100

    yay I figured it out by myself!

--- js-complete-course/06-dom/09-match-password-one/script.js
+++ js-complete-course/06-dom/09-match-password-one/script.js
@@ -11,6 +11,19 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function change() {
+
+        let x = document.getElementById("pass-one").value;
+        console.log(x);
+
+        let y = document.getElementById("pass-two").value;
+        console.log(y);
+
+        if (x !== y) {
+            document.getElementById("pass-one").style.borderColor = "red";
+            document.getElementById("pass-two").style.borderColor = "red";
+        }
+    });
+
 
 })();
