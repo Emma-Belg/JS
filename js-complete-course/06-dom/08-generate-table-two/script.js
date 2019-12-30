f3616b36b9ce45a1a04699d7673268fd74c1f22f
emma <emma.wishart@live.com.au>
2019-12-30 16:18:21 +0100

    thans to sali for her help!

--- js-complete-course/06-dom/08-generate-table-two/script.js
+++ js-complete-course/06-dom/08-generate-table-two/script.js
@@ -4,6 +4,22 @@
  *
  * coded by leny@BeCode
  * started at 26/10/2018
+ *
+ *
+    let c = [];
+    for (i = 0  ; i < 10; i++) {
+        c[i] = document.createElement("TD");
+        document.getElementById("id10").appendChild(c[i]);
+        c[i].innerHTML =  i;
+    }
+    let r = [];
+    for (x = 1; x < 10; x++) {
+        r[x] = document.createElement("TR");
+        document.getElementById("id10").appendChild(r[x]);
+        r[x].innerHTML =  x;
+    }
+
+
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,6 +27,23 @@
 
 (function() {
 
-    // your code here
+    let newtable = document.createElement("TABLE");
+    newtable.setAttribute("id", "id10");
+
+    document.getElementById("target").appendChild(newtable);
+
+    let c = [];
+    for (i = 0  ; i < 10; i++) {
+        c[i] = document.createElement("TD");
+        document.getElementById("id10").appendChild(c[i]);
+        c[i].setAttribute("id", "col" + i);
+        let r = [];
+        for (x = 0; x < 10; x++) {
+            r[x] = document.createElement("TR");
+            document.getElementById("col" + i).appendChild(r[x]);
+            r[x].innerHTML =  (x+1) * (i+1);
+        }
+    }
+    
 
 })();
