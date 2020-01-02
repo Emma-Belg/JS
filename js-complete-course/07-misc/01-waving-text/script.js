0b21d0c2c57abc23768ce811f9ec1f57a342ab43
emma <emma.wishart@live.com.au>
2020-01-02 15:06:47 +0100

    adding partial while it isnt broken

--- js-complete-course/07-misc/01-waving-text/script.js
+++ js-complete-course/07-misc/01-waving-text/script.js
@@ -4,6 +4,27 @@
  *
  * coded by leny@BeCode
  * started at 26/10/2018
+ *
+ *
+ *     const startTime = new Date().getTime();
+    const waveLetters = document.getElementsByClassName("material");
+    const waveTarget = document.getElementById('target')[0];
+    for (let i = 0; i < waveLetters.length; i++) {
+        function wave (${ waveLetters[i] }).appendTo(waveTarget);
+    }
+    function runWave() {
+        const width = $(window).width();
+        const letterSpacing = width > 900 ? 24 : 16;
+        const elapsed = new Date().getTime() - startTime;
+        const pos = elapsed * 0.05;
+        wave ('.animated-wave .letter').each(function(index, letter) {
+            const posx = width - (pos - letterSpacing * index) % width;
+            const posy = Math.sin(posx / 40) * 20;
+            $(letter).css('left', posx + 'px');
+            $(letter).css('top', posy + 'px');
+        });
+    }
+    setInterval(runWave, 30);
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,6 +32,38 @@
 
 (function() {
 
-    // your code here
+    //get text from HTML content. '.textContent' is needed to get the text out of it
+    let text = document.getElementById("target");
+    let textCont = text.textContent;
+
+
+    // split the text into individual letters. This is the element that needs to be wrapped later
+    let letters = textCont.split("");
+    console.log(letters);
+    let wrap = [];
+
+
+    for (let i=0; i < letters.length; i++) {
+
+      //  for (let l=0; l < letters.length; l+5){}
+        //creating element to wrap each letter
+        wrap[i] = document.createElement("span");
+        //con the below so that I can use if for the style size
+        let size = "font-size:"+[i]*2+"px";
+        wrap[i].setAttribute("style", size);
+        let newSpan = document.createTextNode(letters[i]);
+        wrap[i].appendChild(newSpan);
+        text.appendChild(wrap[i]);
+    }
+
+    
+
+    // insert wrapper before el in the DOM tree
+    //letters.prepend(wrap, letters);
+
+    //move letters into wrapper
+    //wrap.appendChild(letters);
+
+
 
 })();
