c397c7926d2b0b3424d07fb0ffa1b414bee1f734
emma <emma.wishart@live.com.au>
2020-01-08 17:05:51 +0100

    thanks to Sali and Sicco for the help!

--- js-complete-course/09-fetch/02-list-to-template/script.js
+++ js-complete-course/09-fetch/02-list-to-template/script.js
@@ -10,5 +10,41 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    let dataElement;
+
+        fetch('http://localhost:12345/_shared/api.json')
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+               dataElement = myJson.heroes;
+               test(dataElement);
+            });
+function test (anything){
+    console.log(anything);
+    console.log(anything[0].name);
+}
+
+    let template = document.getElementById("tpl-hero");
+
+    //console.log(dataElement[0].alterEgo);
+
+    document.getElementById("run").addEventListener("click", function () {
+
+       //console.log(clone);
+
+        dataElement.forEach(function (element, index) {
+
+            let clone = template.content.cloneNode(true);
+            clone.querySelector(".name").innerHTML = element.name;
+            clone.querySelector(".alter-ego").innerHTML = element.alterEgo;
+
+
+            document.getElementById("target").appendChild(clone);
+
+        });
+
+    });
+
 })();
